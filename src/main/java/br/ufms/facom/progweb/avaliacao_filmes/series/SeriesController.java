91eb89a0449680511b9753c6f2cf35b413f3ee90
unknown <davi.m.vargas@ufms.br>
2025-06-23 00:08:29 -0400

    ajustando para aparecer series na tela de series

--- src/main/java/br/ufms/facom/progweb/avaliacao_filmes/series/SeriesController.java
+++ src/main/java/br/ufms/facom/progweb/avaliacao_filmes/series/SeriesController.java
@@ -1,5 +1,7 @@
 package br.ufms.facom.progweb.avaliacao_filmes.series;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,17 +12,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-@RequestMapping("/series")
+@RequestMapping("/api/series")
 public class SeriesController {
 
     @Autowired
     private SeriesService service;
 
-    @GetMapping("/info") 
-    public Iterable<Series> listarTodos() {
-        return service.listarTodos();
+    @GetMapping("/listar")
+    public List<SeriesCardDto> listarTodos() {
+        return service.buscarTodasAsSeries();
     }
+
+    //@GetMapping("/info") 
+    //public Iterable<Series> listarTodos() {
+     //   return service.listarTodos();
+    //}
 
     @GetMapping("/contar")
     public long contarPorGenero(String genero) {
--- src/main/java/br/ufms/facom/progweb/avaliacao_filmes/series/SeriesService.java
+++ src/main/java/br/ufms/facom/progweb/avaliacao_filmes/series/SeriesService.java
@@ -1,7 +1,13 @@
 package br.ufms.facom.progweb.avaliacao_filmes.series;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.ufms.facom.progweb.avaliacao_filmes.filmes.Filmes;
+import br.ufms.facom.progweb.avaliacao_filmes.filmes.FilmesCardDto;
 
 @Service
 public class SeriesService {
@@ -39,5 +45,32 @@ public class SeriesService {
     
     public void excluirSerie(String id) {
         repository.deleteById(id);
+    }
+
+    private SeriesCardDto converterParaCardDto(Series serie){
+        SeriesCardDto dto = new SeriesCardDto();
+        dto.setId(serie.getId());
+        dto.setTitulo(serie.getTitulo());
+        dto.setGenero(serie.getGenero());
+        dto.setImagem(serie.getImagem());
+        dto.setDiretor(serie.getDiretor());
+        dto.setTemporadas(serie.getTemporadas());
+        dto.setAnoLancamento(serie.getAnoLancamento());
+        dto.setSinopse(serie.getSinopse());
+        double mediaAvaliacoes = serie.getAvaliacoes().stream()
+            .mapToDouble(avaliacao -> avaliacao.getNota())
+            .average()
+            .orElse(0.0);
+        dto.setMediaAvaliacoes(mediaAvaliacoes);
+        dto.setAvaliacoes(serie.getAvaliacoes());
+        dto.setTipo(serie.getTipo());
+        return dto;
+    }
+
+    public List<SeriesCardDto> buscarTodasAsSeries(){
+        List<Series> series = (List<Series>) repository.findAll();
+        return series.stream()
+            .map(this::converterParaCardDto)
+            .collect(Collectors.toList());
     }
 }
